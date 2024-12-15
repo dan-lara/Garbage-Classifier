ddf1b16e84c58fef2ddbf3ad32d826d1037f0c3a
Daniel Ferreira Lara <d2021003661@unifei.edu.br>
2024-12-15 22:55:00 +0100

    Add normalization to custom_transform for improved image preprocessing

--- app.py
+++ app.py
@@ -109,6 +109,7 @@ custom_classes = ["🔋 Batterie", "📦 Carton", "🔗 Metal", "🍓 Organique"
 custom_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 # custom_transform = transforms.Compose([
 #     transforms.RandomRotation(20),
